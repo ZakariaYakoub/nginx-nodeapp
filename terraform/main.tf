624c921ba93c40a362f32261e4d435dffdaf375f
Zakaria Yakoub <zakaria.ait.yakoub@gmail.com>
2023-05-15 17:46:46 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -90,6 +90,14 @@ resource "aws_security_group" "myapp-sg" {
         cidr_blocks = ["0.0.0.0/0"]
 
     }
+    
+    ingress {
+        from_port = 3000
+        to_port = 3000
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+
+    }
 
     egress {
         from_port = 0
